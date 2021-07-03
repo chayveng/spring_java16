358a61749b96960f318dc7a6ee439a2c5b823c14
chayveng <chayveng@outlook.com>
2021-07-03 20:51:16 +0700

    demo api

---
+++ src/test/java/com/example/spring_java16/SpringJava16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_java16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJava16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
